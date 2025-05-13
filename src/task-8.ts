b20de67b4bf74b6b875618b7fa701323c6ba7f96
Yuri Boblak <sseslesnes@gmail.com>
2025-05-13 09:10:38 +0300

    Angle brackets copy paste mentor warning

--- src/task-8.ts
+++ src/task-8.ts
@@ -16,7 +16,7 @@ interface Post {
 import axios from "axios";
 
 async function fetchPosts(): Promise<Post[]> {
-  const response = await axios.get<Post[]>("<https://jsonplaceholder.typicode.com/posts>");
+  const response = await axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
   return response.data;
 }
 
